998d9e11742a4fed42d3bd806fbeb59a03f121a1
abhishek sharma <abhishek.sharma@codebrewinnovations.com>
2023-09-05 12:33:59 +0530

    second commit

--- controllers/authCotroller.js
+++ controllers/authCotroller.js
@@ -12,7 +12,7 @@ const createToken = (id) => {
 }
 
 const authenticateToken=(req, res, next)=> {
-  const token = req.header('Authorization');
+  const token = req.header('Authorization'); 
   if (!token) return res.status(401).send('Access denied.');
 
   jwt.verify(token, 'secret-key', (err, user) => {
